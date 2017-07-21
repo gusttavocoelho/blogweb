bd6033011c49fd870f88941e01066d6e2c1a63ea
Gustavo <gustavo_coelho97@hotmail.com>
2017-07-20 21:35:43 -0300

    Adicionada permissao para hosts

--- projeto/settings.py
+++ projeto/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '#e8@qmdjowbf430y2xvf@cp^n-gi(iy-+yn5!-g*su5iuagubs'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['gusttavocoelho.pythonanywhere.com', '127.0.0.1']
 
 
 # Application definition
